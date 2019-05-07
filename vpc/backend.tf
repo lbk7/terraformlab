f55b10943e38f1be9ee7c6c8246a96f2cd7824ac
lbk7 <gangstalion@hotmail.fr>
2019-05-07 15:07:35 +0200

    pushing to git

--- vpc/backend.tf
+++ vpc/backend.tf
@@ -1,8 +1,7 @@
 terraform {
-    backend "s3"
-    {
-        bucket = "myonboardings3"
-        key = "vpc\terraform.tfstate"
-        region = "eu-west-1"
-    }
+  backend "s3" {
+    bucket = "myonboardings3"
+    key    = "vpc\terraform.tfstate"
+    region = "eu-west-1"
+  }
 }
